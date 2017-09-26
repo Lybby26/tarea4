69dead3c3a10b96f484e8465160656f73866b9ca
Mac Mini <macmini@Macs-Mac-mini.local>
2017-09-25 22:15:14 -0500

    Cell objects created to m file

---
+++ Tarea4/cellMainTable.h
@@ -0,0 +1,16 @@
+//
+//  cellMainTable.h
+//  Tarea4
+//
+//  Created by Mac Mini on 9/25/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMainTable : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *imgUser;
+@property (strong, nonatomic) IBOutlet UILabel *lblName;
+@property (strong, nonatomic) IBOutlet UILabel *lblAge;
+
+@end
---
+++ Tarea4/cellMainTable.m
@@ -0,0 +1,24 @@
+//
+//  cellMainTable.m
+//  Tarea4
+//
+//  Created by Mac Mini on 9/25/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import "cellMainTable.h"
+
+@implementation cellMainTable
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
